3082648512dbfc5d57e3327bc13180fd5d04552d
kimbso <kimbso@bc.edu>
2016-10-10 15:18:38 -0400

    Some changes

--- app/src/main/java/com/example/roseanna/adultfinance/Calculator.java
+++ app/src/main/java/com/example/roseanna/adultfinance/Calculator.java
@@ -6,6 +6,8 @@ package com.example.roseanna.adultfinance;
 public class Calculator {
     private Bank checking;
     private Bank savings;
+    private Expense allowance;
+    private Expense monthly;
 
     public Calculator(Bank checking, Bank savings) {
         this.checking   = checking;
